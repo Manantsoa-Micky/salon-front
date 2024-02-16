1f19dcb1589e3b37d1de1dec6e2fa22512b7a887
Manantsoa-Micky <mickymickymarc@gmail.com>
2024-02-16 11:27:41 +0300

    tailwind + ngrx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { httpRequestInterceptor } from './_helpers/http.interceptor';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([httpRequestInterceptor])),
-  ],
+    provideStore()
+],
 };
