dd66f1d7ed641f7a1321a62ccced428579791b55
Manantsoa-Micky <mickymickymarc@gmail.com>
2024-02-24 01:46:54 +0300

    signup endpoint working

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -2,11 +2,28 @@ import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ButtonModule } from 'primeng/button';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-add-employee',
   standalone: true,
-  imports: [ReactiveFormsModule, JsonPipe, DropdownModule],
+  imports: [
+    ReactiveFormsModule,
+    JsonPipe,
+    DropdownModule,
+    InputTextModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputMaskModule,
+    InputNumberModule,
+    ButtonModule,
+  ],
   templateUrl: './add-employee.component.html',
   styleUrl: './add-employee.component.css',
 })
@@ -18,7 +35,26 @@ export class AddEmployeeComponent {
     lastName: [''],
     username: ['', Validators.required],
     adress: [''],
+    role: [''],
+    phone: [''],
+    salary: [],
+    hours: this.formBuilder.group({
+      begin: ['8h00'],
+      end: ['17h00'],
+    }),
   });
-  constructor(private formBuilder: FormBuilder) {}
-  onSubmit() {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {}
+  onSubmit() {
+    this.authService.signup(this.employeeForm.value).subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (err) => {},
+    });
+    console.log(this.employeeForm.value);
+  }
+  roles = ['Manager', 'Employé', 'Client'];
 }
